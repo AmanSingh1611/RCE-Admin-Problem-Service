32af529193202b2de471c2dc7918f7ebe993802b
Aman <aman16024375@gmail.com>
2024-05-29 17:56:00 +0530

    Feat : Added mongoose db config

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -1,7 +1,9 @@
-const dotenv=require('dotenv');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 3000,
+    ATLAS_DB_URL: process.env.ATLAS_DB_URL ,
+    NODE_ENV: process.env.NODE_ENV || "development"
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/server.config');
 const apiRouter = require('./routes');
 const errorHandler = require('./utils/errorHandler');
-
+const connectToDB = require('./config/db.config');
+//const mongoose = require('mongoose');
 
 const app = express();
 
@@ -21,11 +22,18 @@ app.get('/ping', (req, res) => {
     return res.json({message: 'Problem Service is alive'});
 });
 
-
 // last middleware if any error comes
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server started at PORT: ${PORT}`);
-    
+    await connectToDB();
+    console.log("Successfully connected to db");
+
+    // Dummy Testcase
+
+    // const Cat = mongoose.model('Cat', { name: String });
+
+    // const kitty = new Cat({ name: 'Zildjian' });
+    // kitty.save().then(() => console.log('meow'));
 });
